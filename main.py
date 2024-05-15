71f4509a2162a01976c92ab7421487de94935832
ChaniruYaneth <ihcyaneth12@gmail.com>
2024-05-15 14:22:25 +1200

    Testing and trialling to position elements.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from tkinter import *
 
 window=Tk()
+window.geometry("300x300")
+window.title("RightWay")
+
+hello_label = Label(text="Hello, World!")
+hello_label.pack(side="bottom")
 
 window.mainloop()
