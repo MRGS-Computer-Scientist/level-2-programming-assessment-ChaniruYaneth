8459f4ac4c23fd0ace192b2eb9206507e1872606
ChaniruYaneth <ihcyaneth12@gmail.com>
2024-05-18 12:15:49 +1200

    Started coding the login page of the app. Created a function to create an account. Also, to check if passwords match and adding tthem to the users list.

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 from tkinter import *
 import time
+
+def create_account():
+    def submit():
+        username = username_entry.get()
+        password = password_entry.get()
+        confirm_password = confirm_password_entry.get()
+
+        if password == confirm_password:
+            if username in users:
+                message_label.config(text="You already have an account. Please sign-in.")
+            else:
+                users[username] = password
+                message_label. config(text="You have succesfully signed-up.")
+                create_account_window.after(3000, create_account_window.destroy)
+
+        else:
+            message_label.config(text="Passwords do not match. Please try again.")
