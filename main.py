156abd14a607f52f0f64e3f5821a4dc7a846e5c1
ChaniruYaneth <ihcyaneth12@gmail.com>
2024-05-18 12:02:59 +1200

    Delted previous code and imported time.

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
 from tkinter import *
-
-w_width = 500
-w_height = 700
-
-window=Tk()
-window.geometry(str(w_width) + "x" + (w_height))
-window.title("RightWay")
-
-main_frame = Frame(background="red", width=w_width, height=w_height)
-main_frame.pack()
-
-hello_label = Label(text="Hello, World!")
-hello_label.place(x=300, y=300)
-
-window.mainloop()
+import time
